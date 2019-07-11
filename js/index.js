49a33e84cfd06e01669eafc9c70389ef578d96ae
dengny <1028724238@qq.com>
2019-07-11 20:37:44 +0800

    更新了index和html、css文件

--- js/index.js
+++ js/index.js
@@ -2,17 +2,17 @@ var recommend = document.getElementsByClassName("recommend")[0];
 var divs = recommend.children;
 for (var div of divs) {
     div.onmouseover = function() {
-            var div = this;
-            div1 = div.querySelector("p:nth-child(5)");
-            div1.style.height = "0px";
-            div2 = div.querySelector("div:last-child");
-            div2.style.display = "block";
-        }
-        // div.onmouseover = function() {
-        //     var div = this;
-        //     div1 = div.querySelector("p:nth-child(5)");
-        //     div1.style.height = "20px";
-        //     div2 = div.querySelector("div:last-child");
-        //     div2.style.display = "none";
-        // }
+        var div = this;
+        var div1 = div.querySelector("p:nth-child(5)");
+        div1.style.height = "0px";
+        var div2 = div.querySelector("div:last-child");
+        div2.style.display = "block";
+    }
+    div.onmouseout = function() {
+        var div = this;
+        var div1 = div.querySelector("p:nth-child(5)");
+        div1.style.height = "20px";
+        var div2 = div.querySelector("div:last-child");
+        div2.style.display = "none";
+    }
 }
