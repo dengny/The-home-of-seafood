7d1de9ebb534418320c2e3b203ed5033de6f62e4
dengny <1028724238@qq.com>
2019-08-06 19:57:31 +0800

    更新首页及增加图片

--- js/index.js
+++ js/index.js
@@ -21,22 +21,25 @@
         $('<link rel="stylesheet" href="../css/carousel.css"></link>').appendTo("head")
     })
 
-    var recommend = document.getElementsByClassName("recommend")[0];
-    var divs = recommend.children;
-    for (var div of divs) {
-        div.onmouseover = function() {
-            var div = this;
-            var div1 = div.querySelector("p:nth-child(5)");
-            div1.style.height = "0px";
-            var div2 = div.querySelector("div:last-child");
-            div2.style.display = "block";
-        }
-        div.onmouseout = function() {
-            var div = this;
-            var div1 = div.querySelector("p:nth-child(5)");
-            div1.style.height = "20px";
-            var div2 = div.querySelector("div:last-child");
-            div2.style.display = "none";
+    //主体楼层js
+    var recommend = document.getElementsByClassName("recommend");
+    for (var i = 0; i < recommend.length; i++) {
+        var divs = recommend[i].children;
+        for (var div of divs) {
+            div.onmouseover = function() {
+                var div = this;
+                var div1 = div.querySelector("p:nth-child(5)");
+                div1.style.height = "0px";
+                var div2 = div.querySelector("div:last-child");
+                div2.style.display = "block";
+            }
+            div.onmouseout = function() {
+                var div = this;
+                var div1 = div.querySelector("p:nth-child(5)");
+                div1.style.height = "20px";
+                var div2 = div.querySelector("div:last-child");
+                div2.style.display = "none";
+            }
         }
     }
 })()
